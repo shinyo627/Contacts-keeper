08eabc0b176eaa0481b2b0bfc10290e2c5630ef3
shinyo627 <shinyo627@gmail.com>
2021-01-08 00:05:34 +0100

    contact routes have been implemented for Basic CRUD requests functionalities. Basic Backend Custom API for this project is ready.

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,32 +1,128 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../middlewares/auth');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route   GET api/contacts
 // @desc    Get all users contacts
 // @access  Private
-router.get('/', (req, res) => {
-  res.send(`Get a user's all contacts`);
+router.get('/', auth, async (req, res) => {
+  try {
+    // sort({date: -1}) will return recent contacts first in array
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   POST api/contacts
 // @desc    Add new contact
 // @access  Private
-router.post('/', (req, res) => {
-  res.send('Add a new contact');
-});
+router.post(
+  '/',
+  [auth, [check('name', 'Name is required').not().isEmpty()]],
+  async (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        // assigning individual user's mongoDB id to user field in each new contact
+        user: req.user.id,
+      });
+
+      const contact = await newContact.save();
+
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 // @route   PUT api/contacts:id
 // @desc    Update/Edit a contact
 // @access  Private
-router.put('/:id', (req, res) => {
-  res.send('Update/Edit contact');
+router.put('/:id', auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  // Build contact object
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    // req.params.id is individual contact id from mongoDB
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) {
+      return res.status(404).json({ msg: 'Contact not found' });
+    }
+
+    // Make user owns contact
+    if (contact.user.toString() != req.user.id) {
+      return res.status(404).json({ msg: 'Not Authorized' });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      // This params new: true object means if there's no contact in DB then store contactFields as new
+      { new: true }
+    );
+
+    // Returning updated contact
+    res.json(contact);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   DELETE api/contacts/:id
 // @desc    Delete a contact
 // @access  Private
-router.delete('/', (req, res) => {
-  res.send('Delete a contact');
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    // req.params.id is individual contact id from mongoDB
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) {
+      return res.status(404).json({ msg: 'Contact not found' });
+    }
+
+    // Make user owns contact
+    if (contact.user.toString() != req.user.id) {
+      return res.status(404).json({ msg: 'Not Authorized' });
+    }
+
+    await Contact.findByIdAndRemove(req.params.id);
+
+    // Returning updated contact
+    res.json({ msg: 'Contact removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // export Route
