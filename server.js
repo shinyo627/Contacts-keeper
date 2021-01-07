25fd913d7d29e4af313229af71be08458011360e
shinyo627 <shinyo627@gmail.com>
2021-01-07 16:13:03 +0100

    MongoDB connection, UserSchemaModel, user register API route have been implemented that returns  JWT Token for future protected routes

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Initialize Middleware for accepting incoming req.body
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the ContactsKeeper API' })
 );
